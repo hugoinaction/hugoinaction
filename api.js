ecab9dc9009f53bb72332ebf818acdede80a9600
Atishay Jain <contact@atishay.me>
2020-12-04 23:10:48 -0800

    Added latex2svg

---
+++ api.js
@@ -0,0 +1,41 @@
+const http = require("http");
+const querystring = require("querystring");
+const latex2svg = require("./api/latex2svg");
+
+const port = process.env.PORT || 3000;
+const server = http.createServer().listen(port);
+
+server.on("request", async function (req, res) {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const queryStringParameters =
+    url.search && querystring.parse(url.search.slice(1));
+  const request = {
+    queryStringParameters,
+    path: url.pathname,
+    httpMethod: req.method,
+    headers: req.headers,
+    body: req.body,
+  };
+  let response = {
+    statusCode: 404,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ error: "Page not found" }),
+  };
+
+  try {
+    switch (url.pathname) {
+      case "/latex2svg":
+        response = await latex2svg.handler(request);
+        break;
+    }
+  } catch (e) {
+    response.statusCode = 500;
+    console.log(e);
+    response.body = JSON.stringify(e);
+  }
+
+  res.writeHead(response.statusCode, response.headers);
+  res.end(response.body);
+});
+
+console.log(`Listening on port ${port}`);
