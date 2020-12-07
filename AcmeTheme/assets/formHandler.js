1b88219e1c6fb7f041ec3423cc6dd5f83e448b91
Atishay Jain <contact@atishay.me>
2020-12-06 22:11:22 -0800

    10.4.2: Splitting the JavaScript code into multiple modules

---
+++ AcmeTheme/assets/formHandler.js
@@ -0,0 +1,27 @@
+export default {
+  init() {
+    document.querySelectorAll("[data-dynamic-form]")
+      .forEach(async form => form.addEventListener('submit', async event => {
+        event.preventDefault();
+        const form = event.target;
+        const data = new FormData(form);
+        const action = form.action;
+        const method = form.method;
+        const response = await fetch(action, {
+          method,
+          body: data,
+          headers: { "Accept": "application/json" }
+        });
+        if (response.ok) {
+          form.insertAdjacentHTML('afterend',
+            document.querySelector(form.dataset.success).innerHTML);
+        } else {
+          form.insertAdjacentHTML('afterend',
+            document.querySelector(form.dataset.error).innerHTML);
+        }
+        const message = form.nextElementSibling;
+        form.reset();
+        setTimeout(() => message.remove(), 10000);
+      }));
+  }
+};
--- AcmeTheme/assets/index.js
+++ AcmeTheme/assets/index.js
@@ -1,31 +1,13 @@
-function addFormHandler() {
-  document.querySelectorAll("[data-dynamic-form]")
-    .forEach(async form => form.addEventListener('submit', async event => {
-      event.preventDefault();
-      const form = event.target;
-      const data = new FormData(form);
-      const action = form.action;
-      const method = form.method;
-      const response = await fetch(action, {
-        method,
-        body: data,
-        headers: { "Accept": "application/json" }
-      });
-      if (response.ok) {
-        form.insertAdjacentHTML('afterend',
-          document.querySelector(form.dataset.success).innerHTML);
-      } else {
-        form.insertAdjacentHTML('afterend',
-          document.querySelector(form.dataset.error).innerHTML);
-      }
-      const message = form.nextElementSibling;
-      form.reset();
-      setTimeout(() => message.remove(), 10000);
-    }));
+// AcmeTheme/assets/index.js
+import FormHandler from "formHandler"
+
+function init() {
+  FormHandler.init();
+
 }
 
 if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', addFormHandler);
+  document.addEventListener('DOMContentLoaded', init);
 } else {
-  addFormHandler();
+  init();
 }
