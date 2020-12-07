5af3ea736b35831b19b55668c5f0aff168401e4d
Atishay Jain <contact@atishay.me>
2020-12-06 22:11:59 -0800

    11.2.4: Creating a function for LaTeX to SVG conversion

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Hugo In Action
 ===============
 
-This code corresponds to section **10.6** of Hugo in Action.
+This code corresponds to section **11.2.4** of Hugo in Action.
 
-You can see this live at https://ch10-8.hugoinaction.com.
+You can see this live at https://ch11-1.hugoinaction.com.
 
-You can see the changes made in this branch at https://github.com/hugoinaction/hugoinaction/compare/ch10-7...ch10-8
+You can see the changes made in this branch at https://github.com/hugoinaction/hugoinaction/compare/ch10-8...ch11-1
 
---
+++ api/latex2svg.js
@@ -0,0 +1,54 @@
+const MathjaxModule = require("mathjax");
+
+let MathJax = null;
+module.exports = {
+  /**
+   * Function to handle calls to the API endpoint of the cloud function.
+   */
+  async handler(event, context) {
+    if (
+      !event.queryStringParameters ||
+      !process.env.LATEX2SVG_PASSWORD ||
+      event.queryStringParameters.password !== process.env.LATEX2SVG_PASSWORD
+    ) {
+      return {
+        statusCode: 401,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          error: "Access Denied.",
+        }),
+      };
+    }
+    if (!event.queryStringParameters || !event.queryStringParameters.tex) {
+      return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          error: "The required `tex` parameter not supplied.",
+        }),
+      };
+    }
+    if (!MathJax) {
+      MathJax = await MathjaxModule.init({
+        loader: { load: ["input/tex", "output/svg"] },
+      });
+    }
+
+    const svg = MathJax.tex2svg(event.queryStringParameters.tex, {
+      display: event.queryStringParameters.display,
+      em: event.queryStringParameters.em,
+      ex: event.queryStringParameters.ex,
+      containerWidth: event.queryStringParameters.containerWidth,
+      lineWidth: event.queryStringParameters.lineWidth,
+      scale: event.queryStringParameters.scale,
+    });
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data: MathJax.startup.adaptor.outerHTML(svg),
+      }),
+    };
+  },
+};
