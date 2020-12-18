97f7d2c402dc8d51002201e962e356ac1c182a0e
Atishay Jain <contact@atishay.me>
2020-12-17 22:01:36 -0800

    Add API to read file.

---
+++ api/checkout.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+module.exports = {
+  /**
+   * Function to handle calls to the API endpoint of the cloud function.
+   */
+  async handler(event, context) {
+    const data = await fs.readFile(__dirname, "../assets/products.csv", {encoding: 'utf-8'});
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        data,
+      }),
+    };
+  },
+};
