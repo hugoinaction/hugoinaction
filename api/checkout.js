d2af3d5655e8c73b4e6532a433896b37d184ca10
Atishay Jain <contact@atishay.me>
2020-12-17 22:18:34 -0800

    Fix.

--- api/checkout.js
+++ api/checkout.js
@@ -5,7 +5,7 @@ module.exports = {
    * Function to handle calls to the API endpoint of the cloud function.
    */
   async handler(event, context) {
-    const data = await fs.readFile(__dirname, "../assets/products.csv", {encoding: 'utf-8'});
+    const data = await fs.readFile(__dirname + "/../assets/products.csv", {encoding: 'utf-8'});
 
     return {
       statusCode: 200,
