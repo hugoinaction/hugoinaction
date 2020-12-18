58670704834c804048f1b3ddfe91bcaeda7cacb1
Atishay Jain <contact@atishay.me>
2020-12-17 22:56:39 -0800

    Adding fetch for checkout.

--- api/checkout.js
+++ api/checkout.js
@@ -1,11 +1,12 @@
 const fs = require("fs").promises;
-
+const env = require("./env.json");
+const fetch = require("node-fetch");
 module.exports = {
   /**
    * Function to handle calls to the API endpoint of the cloud function.
    */
   async handler(event, context) {
-    const data = await fs.readFile(__dirname + "/../assets/products.csv", {encoding: 'utf-8'});
+    const data = await fetch(`${env.DEPLOY_PRIME_URL}/store/index.json`);
 
     return {
       statusCode: 200,
