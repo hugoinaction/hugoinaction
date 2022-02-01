725ee6adf7ea96858d5c73160880e24513c5fc19
Atishay Jain <contact@atishay.me>
2022-02-01 10:06:51 -0800

    12.4: Enable email based fulfillment

--- api/postinstall.js
+++ api/postinstall.js
@@ -15,4 +15,18 @@ const path = require('path');
   ////////////////////////////////////////////////////////////////
   // End custom code for getting build details from Netlify.
   ////////////////////////////////////////////////////////////////
+
+  const data = {};
+
+  const images = await fs.readdir(__dirname + "/assets");
+  await Promise.all(images.map(async image => {
+    const extname = path.extname(image);
+    if (extname === '.png') {
+      const buffer = await fs.readFile(__dirname + "/assets/" + image);
+      data[path.basename(image, path.extname(image))] = buffer.toString('base64');
+    }
+  }));
+
+  await fs.writeFile(__dirname + "/result.json", JSON.stringify(data, null, 2));
+
 })();
